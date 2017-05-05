693609d3815b3ef9fb52cec095becdd05854fe91
petitviolet <violethero0820@gmail.com>
2017-05-05 22:38:07 +0900

    update sample

--- src/hoge.erl
+++ src/hoge.erl
@@ -1,7 +1,6 @@
 -module(hoge).
 
--export([do/0, doMap/1, doDouble/0]).
-
+-export([do/0, doMap/1, doDouble/0, tryF/1]).
 
 do() -> sample:add(1, 2).
 
@@ -10,5 +9,15 @@ doMap(F) -> lists:map(F, [1, 2, 3]).
 double(X) -> X * 2.
 
 doDouble() -> lists:map(fun double/1, [1, 2, 3]).
+
+tryF(F) ->
+  try(F()) of
+    true -> yes;
+    _ -> no
+  catch
+    throw: Throw -> {caught_throw, Throw};
+    error: Error -> {caught_error, Error};
+    exit: Exit -> {caught_exit, Exit}
+  end.
 
 
--- src/sample.erl
+++ src/sample.erl
@@ -1,6 +1,26 @@
 -module(sample).
 
--export([add/2]).
+-export([add/2, f/2]).
 
 add(0, 0) -> 0;
 add(I, J) -> I + J.
+
+f(0, 0) -> 1;
+f(0, _) -> 2;
+f(_, 0) -> 3;
+f(I, _) when I rem 2 == 0, I rem 3 == 0 -> 4; % andalso
+f(_, J) when J rem 2 == 0; J rem 3 == 0 -> 5; % orelse
+% f(I, J) ->
+%   case I + J of
+%     10 -> 10;
+%     11 -> 11;
+%     N when N < 100 -> N;
+%     _ -> 999
+%   end.
+f(I, J) ->
+  if I + J =:= 10 -> 10;
+     I + J =:= 10 -> 11;
+     I + J < 100 -> I + J;
+     true -> 999
+  end.
+
