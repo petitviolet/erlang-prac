a0040ee36e7dd3a4a9eafd15b18fecf4f3642106
petitviolet <violethero0820@gmail.com>
2017-05-06 23:48:10 +0900

    add try sample

--- src/hoge.erl
+++ src/hoge.erl
@@ -1,6 +1,6 @@
 -module(hoge).
 
--export([do/0, doMap/1, doDouble/0, tryF/1]).
+-export([do/0, doMap/1, doDouble/0, tryF/1, tryF2/1]).
 
 do() -> sample:add(1, 2).
 
@@ -11,13 +11,26 @@ double(X) -> X * 2.
 doDouble() -> lists:map(fun double/1, [1, 2, 3]).
 
 tryF(F) ->
-  try(F()) of
+  try(F())
+  of
     true -> yes;
-    _ -> no
+    false -> no;
+    _ -> unknown
   catch
     throw: Throw -> {caught_throw, Throw};
     error: Error -> {caught_error, Error};
     exit: Exit -> {caught_exit, Exit}
+    % Exception: Reason -> {caught_exception, Exception, Reason} % catch anything
+  after
+    % in other langhage, `finally`
+    io:format("after!!!~n")
   end.
 
+tryF2(F) ->
+  try(F())
+  catch
+    Exception: Reason ->
+      io:format("StackTrace~n~p~n", [erlang:get_stacktrace()]),
+      {caught_exception, Exception, Reason} % catch anything
+  end.
 
